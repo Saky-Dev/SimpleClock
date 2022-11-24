59ada3f3a2ef9be7e2d226c40ce05107060eacdb
IsaacGH <garciaisaac468@gmail.com>
2022-11-24 16:08:05 -0600

    Stopwatch's JS code rearranged; Record added to HTML; Minimum details added to buttons

--- functionality/stopwatch.js
+++ functionality/stopwatch.js
@@ -1,59 +1,75 @@
+let isPaused  = false
 let stopwatch = undefined
 
 const time  = new Date()
 const clock = ['min', 'sec', 'msc'].map(item => document.querySelector(`#${item}`))
+const saved = []
 
-const restartValues = () => {
+const getNow = () => ({
+  min: time.getMinutes(),
+  sec: time.getSeconds(),
+  msc: time.getMilliseconds() / 10
+})
+
+const saveTime = () => {
+  const now = getNow()
+
+  if (now.min > 0 && now.sec > 0 && now.msc){
+    if (saved.length >= 5)
+      saved.splice(0, 1)
+
+    saved.push(`${
+      Object.keys(now).map(item => `${now[item] < 10 ? `0${now[item]}` : now[item]}`)
+    }`.replaceAll(',', ':'))
+  }
+}
+
+const restartValues = (doUpdate, doPause, doSave) => {
+  clearInterval(stopwatch)
+  stopwatch = undefined
+
+  if (doSave)
+    saveTime()
+
   time.setHours(0)
   time.setMinutes(0)
   time.setSeconds(0)
   time.setMilliseconds(0)
+
+  if (doUpdate)
+    updateStopwatch()
   
-  clearInterval(stopwatch)
-  stopwatch = undefined
+  if (doPause)
+    isPaused = false
 }
 
 const updateStopwatch = () => {
-  const now = {
-    min: time.getMinutes(),
-    sec: time.getSeconds(),
-    msc: time.getMilliseconds() / 10
-  }
+  const now = getNow()
 
   time.setTime(time.getTime() + 10)
-
   Object.keys(now).forEach((item, i) => clock[i].innerHTML = now[item] < 10 ? `0${now[item]}` : now[item])
 }
 
-const pauseText = isPause => document.querySelector('#start span').innerHTML = isPause ? 'Continue' : 'Start'
+document.querySelector('#restart').addEventListener('click', () => restartValues(true, true, false))
 
-document.querySelector('#restart').addEventListener('click', () => {
-  pauseText(false)
-  restartValues()
-  updateStopwatch()
-})
-
-document.querySelector('#stop').addEventListener('click', () => {
-  pauseText(false)
-  restartValues()
-})
+document.querySelector('#stop').addEventListener('click', () => restartValues(false, true, true))
 
 document.querySelector('#pause').addEventListener('click', () => {
-  if (stopwatch !== undefined) {
-    pauseText(true)
+  if (!stopwatch && isPaused) {
+    stopwatch = setInterval(updateStopwatch, 10)
+    isPaused = !isPaused
+  } else if (stopwatch && !isPaused) {
     clearInterval(stopwatch)
     stopwatch = undefined
+    isPaused = !isPaused
   }
 })
 
 document.querySelector('#start').addEventListener('click', () => {
-  pauseText(false)
-
-  if (stopwatch === undefined)
+  if (stopwatch === undefined){
     stopwatch = setInterval(updateStopwatch, 10)
+    isPaused = false
+  }
 })
 
-;(() => {
-  restartValues()
-  updateStopwatch()
-})()
+;(() => restartValues(true, false, false))()
